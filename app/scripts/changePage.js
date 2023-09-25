f3fcb4beaf269b9fca8d7d0e852087666a791877
oskarBoladz <103318195+oskarBoladz@users.noreply.github.com>
2023-09-25 16:04:42 +0200

    .

--- app/scripts/changePage.js
+++ app/scripts/changePage.js
@@ -1,16 +1,16 @@
 
-pages = {"wiki":0,
+pages = {"wiki":2,
         "ui":1,
-        "picker":2,
-        "color":3,
-        "pallet":4};
+        "picker":0,
+        "color":-1,
+        "pallet":-2};
 page=2;
-document.getElementById("pageContainer").style.transform="translateX(-"+2*document.getElementsByClassName('page')[0].offsetWidth+"px)"
+// document.getElementById("pageContainer").style.transform="translateX(-"+2*document.getElementsByClassName('page')[0].offsetWidth+"px)"
 pageChange =(e)=>{
     page = pages[e]
     // console.log(val*document.getElementsByClassName('page')[0].offsetWidth)
-    document.getElementById("pageContainer").style.transform="translateX(-"+page*document.getElementsByClassName('page')[0].offsetWidth+"px)"
+    document.getElementById("pageContainer").style.transform="translateX("+page*document.getElementsByClassName('page')[0].offsetWidth+"px)"
 }
 onresize = (event) => {
-    document.getElementById("pageContainer").style.transform="translateX(-"+page*document.getElementsByClassName('page')[0].offsetWidth+"px)"
+    document.getElementById("pageContainer").style.transform="translateX("+page*document.getElementsByClassName('page')[0].offsetWidth+"px)"
 };
